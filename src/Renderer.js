01be46623bf3bfb930190045bfe26c511228f0c3
Ricardo Havill <rhavill2002@yahoo.com>
2016-02-13 22:00:50 -0500

    Show axes.

--- src/Renderer.js
+++ src/Renderer.js
@@ -5,13 +5,18 @@ export default class Renderer {
 
     constructor(rootElement) {
         this.scene = new THREE.Scene();
+
         this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
         this.camera.position.set(0,45,60);
         this.camera.lookAt(this.scene.position);
-        let light = new THREE.DirectionalLight(0xffffff, 2);
 
+        let light = new THREE.DirectionalLight(0xffffff, 2);
         light.position.set(0,45,60);
         this.scene.add(light);
+
+        let axes = new THREE.AxisHelper(5);
+        this.scene.add( axes );
+
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(WIDTH, HEIGHT);
         rootElement.parentNode.replaceChild(this.renderer.domElement, rootElement);
