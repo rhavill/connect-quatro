8c61bb17269f0fb24a534d8c6f9b5b4fe5062bfa
Ricardo Havill <rhavill2002@yahoo.com>
2016-02-15 14:44:49 -0500

    Added camera controls..

--- src/scene/Scene.js
+++ src/scene/Scene.js
@@ -1,15 +1,14 @@
 import {WIDTH, HEIGHT, CHECKER_RADIUS, CHECKER_HEIGHT}  from './constants'
 
 import THREE from 'three'
+var OrbitControls = require('three-orbit-controls')(THREE)
+
 import Table from './Table'
 import Board from './Board'
 import Camera from './Camera'
 import CheckerFactory from './CheckerFactory'
-
 import Light from './Light'
 import Room from './Room'
-
-//import OrbitControls from './orbit'
 
 export default class Scene {
 
@@ -20,25 +19,13 @@ export default class Scene {
         this.scene.add(new Room().sceneObject);
         this.scene.add(new Table().sceneObject)
         this.scene.add(new Board(CHECKER_RADIUS, CHECKER_HEIGHT).sceneObject)
+        this.controls = new OrbitControls(this.camera.sceneObject)
 
         let axes = new THREE.AxisHelper(5);
         this.scene.add( axes );
         let checkerfactory = new CheckerFactory(CHECKER_RADIUS, CHECKER_HEIGHT);
         let checker = checkerfactory.make('red');
         this.scene.add(checker.sceneObject);
-
-        //this.controls = new OrbitControls(this.camera)
-
-        var cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xF07020 } );
-        // base
-        var cube;
-        cube = new THREE.Mesh(
-        new THREE.CubeGeometry(5, 5, 5 ), cubeMaterial );
-        cube.position.x = -15;	// (20+32) - half of width (20+64+110)/2
-        cube.position.y = 15;	// half of height
-        //cube.position.z = 0;	// centered at origin
-        this.scene.add( cube );
-        this.cube = cube
 
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(WIDTH, HEIGHT);
@@ -50,7 +37,6 @@ export default class Scene {
     }
 
     animate() {
-        this.cube.rotation.y += .05
         window.requestAnimationFrame(this.animate.bind(this));
         this.render();
     }
