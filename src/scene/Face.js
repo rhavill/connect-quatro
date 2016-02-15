8607161bbcf3f596acec36fbbec3615a9d73db35
Ricardo Havill <rhavill2002@yahoo.com>
2016-02-15 07:47:46 -0500

    Added example of shape with hole.

--- src/scene/Face.js
+++ src/scene/Face.js
@@ -3,8 +3,8 @@ import FaceSquare from './FaceSquare'
 export default class Face {
     constructor(material, checkerRadius, checkerHeight) {
         this.sceneObject = new THREE.Group()
-        for (let row = 0; row < 6; row++) {
-            for (let column = 0; column < 7; column++) {
+        for (let row = 0; row < 1; row++) {
+            for (let column = 0; column < 1; column++) {
                 let x = column * checkerRadius * 2
                 let y = row * checkerRadius * 2
                 this.sceneObject.add(new FaceSquare(material, checkerRadius, x, y).sceneObject)
--- src/scene/FaceSquare.js
+++ src/scene/FaceSquare.js
@@ -2,24 +2,57 @@
 export default class FaceSquare {
     constructor(material, checkerRadius, x, y) {
         let length = checkerRadius*2, width = checkerRadius*2;
-
+length=4; width=4;
         let shape = new THREE.Shape();
-        shape.moveTo( 0,0 );
-        shape.lineTo( 0, width );
-        shape.lineTo(length, width );
-        shape.lineTo( length, 0 );
-        shape.lineTo( 0, 0 );
+        shape.moveTo( -2,-2 );
+        shape.lineTo( -2,2 );
+        shape.lineTo(2,2 );
+        shape.lineTo( 2,-2 );
+        shape.lineTo( -2,-2 );
+        //let holeRadius = checkerRadius*.4
+        //shape.moveTo( 0, checkerRadius* 2);
+        //shape.quadraticCurveTo( holeRadius, holeRadius, holeRadius, 0 );
+        //shape.quadraticCurveTo( holeRadius, -holeRadius, 0, -holeRadius );
+        //shape.quadraticCurveTo( -holeRadius, -holeRadius, -holeRadius, 0 );
+        //shape.quadraticCurveTo( -holeRadius, holeRadius, 0, holeRadius );
 
-        let curve = new THREE.EllipseCurve(
-            checkerRadius,  checkerRadius,            // ax, aY
-            checkerRadius*0.85, checkerRadius *.85,           // xRadius, yRadius
-            0,  2 * Math.PI,  // aStartAngle, aEndAngle
-            false,            // aClockwise
-            0                 // aRotation
-        );
-        let path = new THREE.Path( curve.getPoints( 600 ) );
+        //let curve = new THREE.EllipseCurve(
+        //    checkerRadius,  checkerRadius,            // ax, aY
+        //    checkerRadius*0.85, checkerRadius *.85,           // xRadius, yRadius
+        //    0,  2 * Math.PI,  // aStartAngle, aEndAngle
+        //    false,            // aClockwise
+        //    0                 // aRotation
+        //);
+        //let path = new THREE.Path( curve.getPoints( 600 ) );
 
-        shape.holes = [path]
+        let hole = new THREE.Shape();
+
+        //hole.moveTo(0,2);
+        //hole.ellipse(0,0,2,2,0,2*Math.PI)
+
+        let holePoints = [
+                new THREE.Vector3(-1,-1,0),
+                new THREE.Vector3(1,-1,0),
+                new THREE.Vector3(1,1,0),
+                new THREE.Vector3(-1,1,0)
+            ];
+        hole.fromPoints(holePoints);
+
+        var ellipse = new THREE.EllipseCurve(2, 2, 1, 1, 0, 2.0 * Math.PI, false);
+        var ellipsePath = new THREE.CurvePath();
+        ellipsePath.add(ellipse);
+        var ellipseGeometry = ellipsePath.createPointsGeometry(100);
+        var line = new THREE.Line(ellipseGeometry, material);
+
+        var circleRadius = 1;
+        var circleShape = new THREE.Path();
+        circleShape.moveTo( 0, circleRadius );
+        circleShape.quadraticCurveTo( circleRadius, circleRadius, circleRadius, 0 );
+        circleShape.quadraticCurveTo( circleRadius, -circleRadius, 0, -circleRadius );
+        circleShape.quadraticCurveTo( -circleRadius, -circleRadius, -circleRadius, 0 );
+        circleShape.quadraticCurveTo( -circleRadius, circleRadius, 0, circleRadius );
+
+        shape.holes = [hole]
 
         let geometry = new THREE.ShapeGeometry(shape);
         let mesh = new THREE.Mesh(geometry, material)
