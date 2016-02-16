ee13a1833141538497bc490517b4b9b64b183aaf
Ricardo Havill <rhavill2002@yahoo.com>
2016-02-15 23:21:00 -0500

    Increased room size and camera view distance.

--- src/scene/Camera.js
+++ src/scene/Camera.js
@@ -2,7 +2,7 @@ import THREE from 'three'
 
 export default class Camera {
     constructor(aspect) {
-        this.sceneObject = new THREE.PerspectiveCamera(45, aspect, 0.1, 1000);
+        this.sceneObject = new THREE.PerspectiveCamera(45, aspect, 0.1, 10000);
         this.sceneObject.position.set(0,40,150);
         this.sceneObject.lookAt(0,0,0)
     }
--- src/scene/Room.js
+++ src/scene/Room.js
@@ -2,7 +2,7 @@ import THREE from 'three'
 
 export default class Room {
     constructor() {
-        const geometry = new THREE.CubeGeometry( 1000, 1000, 1000 );
+        const geometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
         const material = new THREE.MeshBasicMaterial( { color: 0xD3D3DF, side: THREE.BackSide } );
         this.sceneObject = new THREE.Mesh(geometry, material );
     }
