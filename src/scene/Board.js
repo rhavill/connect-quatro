e3934fc461cd475c25416f3c63051919b92e225c
Ricardo Havill <rhavill2002@yahoo.com>
2016-02-16 20:31:27 -0500

    Added bottom piece to game board.

--- src/scene/Board.js
+++ src/scene/Board.js
@@ -10,6 +10,16 @@ export default class Board {
         this.sceneObject.add(new Slats(checkerRadius, checkerHeight).sceneObject)
         this.sceneObject.add(new Faces(checkerRadius, checkerHeight).sceneObject)
         this.sceneObject.add(new Legs(checkerRadius, checkerHeight).sceneObject)
+
+        let material = new THREE.MeshPhongMaterial( { color: 0x558CC4, specular: 0x606060, side: THREE.DoubleSide } )
+        let geometry = new THREE.BoxGeometry(2*checkerHeight, 14*checkerRadius, checkerHeight)
+        let bottom = new THREE.Mesh(geometry, material)
+        bottom.rotation.x = Math.PI/2
+        bottom.rotation.z = Math.PI/2
+        bottom.position.y += checkerRadius*1.9
+        bottom.position.x += checkerRadius*7
+        this.sceneObject.add(bottom)
+
         this.sceneObject.position.y += checkerRadius*2
         this.sceneObject.position.x -= checkerRadius*7
     }
